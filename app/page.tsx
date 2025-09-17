3137e8342b3affebbcbd2476fad76ff8c89076c9
raymondidu <blinksbreezy@gmail.com>
2025-09-17 22:15:58 +0100

    ca update position

--- app/page.tsx
+++ app/page.tsx
@@ -89,10 +89,11 @@ import Link from "next/link";
                Get your first Grandma Verdict in 5 seconds
               </p>
             </div>
+             <h1 className="text-[12px] lg:text-xl font-bold text-[#8359db]">CA: EG4wSzwjr9ZRCcwU9ryKo6hDR76QgxYrk2SD6a78pump</h1>
             <img src="/herodesk.png" alt="hero desktop image" className="hidden lg:block"/>
             <img src="/heromobile.png" alt="hero desktop image" className="lg:hidden " />
             
-            <h1 className="text-[12px] lg:text-xl font-bold">CA: EG4wSzwjr9ZRCcwU9ryKo6hDR76QgxYrk2SD6a78pump</h1>
+           
             {/* CTA Buttons */}
             <div className="translate-y-[-1rem] animate-fade-in  [--animation-delay:600ms] flex flex-col sm:flex-row items-center gap-4 sm:gap-6 mt-4 sm:mt-6">
               <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
